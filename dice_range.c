8e7ff5d269272238d5aaefe0c5bbe5c7bfe26391
Jemma Chan <106141483+dokkaebabi@users.noreply.github.com>
2023-03-07 18:29:08 +1100

    Update and rename Print_Mouse to dice_range.c

---
+++ dice_range.c
@@ -0,0 +1,35 @@
+// This program reads the number of sides on a set of dice 
+// and how many of them are being rolled
+// Coded by Jemma Chan z5383619 
+// Date: 02/03/2023
+
+#include <stdio.h>
+
+int main(void) {
+    int Dice_Side;
+    int Dice_Roll;
+    int low;
+    int high;
+    double average;
+
+    // scanning the dice values
+    printf("Enter the number of sides on your dice: ");
+    scanf("%d", &Dice_Side);
+    printf("Enter the number of dice being rolled: ");
+    scanf("%d", &Dice_Roll);
+
+    // compute the range 
+    low = Dice_Roll;
+    high = Dice_Side * Dice_Roll;
+
+    // compute the average 
+    average = (low + high) / 2.0; 
+
+    if (Dice_Side <= 0 || high <= 0) {
+        printf("These dice will not produce a range.\n");
+    } else {
+        printf("Your dice range is %d to %d.\n", low, high);
+        printf("The average value is %lf\n", average);
+    }
+    return 0;
+}
